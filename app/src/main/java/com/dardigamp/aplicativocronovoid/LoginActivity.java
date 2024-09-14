a9bd5aefdba923d20aac9c3294bebcfbac652c9c
Jose Ñiquen <j.armando0807@gmail.com>
2024-09-13 19:22:23 -0500

    init: primer avance - metro de lima

---
+++ app/src/main/java/com/dardigamp/aplicativocronovoid/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.dardigamp.aplicativocronovoid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.length()==0 || password.length()==0){
+
+                    Toast.makeText(getApplicationContext(), "Porfavor Complete todos los Detalles",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Inicio de Sesión Exitoso",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dardigamp/aplicativocronovoid/RegisterActivity.java
@@ -0,0 +1,65 @@
+package com.dardigamp.aplicativocronovoid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edEmail, edPassword, edConfirm;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edConfirm = findViewById(R.id.editTextRegConfirmPassword);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textViewExistingUser);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+                if (username.length()==0 || password.length()==0){
+
+                    Toast.makeText(getApplicationContext(), "Porfavor Complete todos los Detalles",Toast.LENGTH_SHORT).show();
+                }else{
+                    if(password.compareTo(confirm)==0){
+
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Confirmación de contraseña no son iguales", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+    }
+}
